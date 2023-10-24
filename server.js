5f8bcae05bfffc105199b9e96e594c42e68a1213
Jenaro Wachira <wjenaro@gmail.com>
2023-10-24 22:58:44 +0300

    goos

--- server.js
+++ server.js
@@ -1,35 +1,52 @@
-const express=require('express');
-const app=express();
-const jwt= require('jsonwebtoken');
-
-
-
+const express = require('express');
+const app = express();
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
 app.use(express.json());
 
-const PORT=3000;
+const PORT = 3000;
 
-const posts= [
+const posts = [
     {
         username: 'jenaro',
         title: "Post 1",
     },
     {
-        username: 'jenaro',
+        username: 'test',
         title: "Post 2",
     },
     {
-        username: 'jenaro',
+        username: 'wachira',
         title: "Post 3",
     },
-]
+];
 
-app.get('/posts', (req, res)=>{
-res.json(posts);
+app.get('/posts', authenticationToken, (req, res) => {
+    res.json(posts.filter(post => post.username === req.user.name)); // Fix the comparison here
 });
-app.post('/login', (req, res)=>{
+
+app.post('/login', async (req, res) => {
+
+
+    const { username, password } = req.body;
     
+    const user = { name: username };
+
+    const accessToken = await jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
+    res.json({ accessToken });
 });
 
-app.listen(PORT, ()=>{
-    console.log(PORT);
-})
+function authenticationToken(req, res, next) {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+    if (token == null) return res.sendStatus(401);
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+        if (err) return res.sendStatus(403);
+        req.user = user; 
+        next();
+    });
+}
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
